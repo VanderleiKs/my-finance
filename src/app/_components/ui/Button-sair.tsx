768a0b469537bb40dfa7e42ebd6c9e23070428fc
Vanderleiks <vanderleiksilva@gmail.com>
2024-11-26 09:22:32 -0300

    fix: removeded font layout (home)

--- src/app/_components/ui/Button-sair.tsx
+++ src/app/_components/ui/Button-sair.tsx
@@ -1,31 +1,32 @@
-import { SignOutButton, useUser } from "@clerk/clerk-react";
-import { LogOut } from "lucide-react";
+import { SignOutButton, useUser } from '@clerk/clerk-react'
+import { LogOut } from 'lucide-react'
+import Image from 'next/image'
 
 export default function LogoutButton() {
-  const { user } = useUser();
+    const { user } = useUser()
 
-  return (
-    <div>
-      {user && (
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <img
-            src={user.imageUrl}
-            alt="Profile"
-            style={{
-              width: 32,
-              height: 32,
-              borderRadius: "50%",
-              marginRight: 8,
-            }}
-          />
-          <span>{user.fullName || user.firstName}</span>
-          <SignOutButton redirectUrl="/login">
-            <button style={{ marginLeft: 8 }} title="Sair">
-              <LogOut className="text-red-500" />
-            </button>
-          </SignOutButton>
+    return (
+        <div>
+            {user && (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Image
+                        src={user.imageUrl}
+                        alt="Profile"
+                        style={{
+                            width: 32,
+                            height: 32,
+                            borderRadius: '50%',
+                            marginRight: 8,
+                        }}
+                    />
+                    <span>{user.fullName || user.firstName}</span>
+                    <SignOutButton redirectUrl="/login">
+                        <button style={{ marginLeft: 8 }} title="Sair">
+                            <LogOut className="text-red-500" />
+                        </button>
+                    </SignOutButton>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    )
 }
